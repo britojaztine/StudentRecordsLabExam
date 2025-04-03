c1a0aa609653bcd4d01b5daf5d7b275dccdb55ec
britojaztine <britojaztine34@gmail.com>
2025-04-03 21:11:13 +0800

    Add files via upload

---
+++ Student_Record_Form.sql
@@ -0,0 +1,8 @@
+INSERT INTO StudentRecordTB 
+(studentId, firstName, lastName, middleName, houseNo, brgyName, municipality, province, region, country, birthdate, age, studContactNo, emailAddress, guardianFirstName, guardianLastName, hobbies, nickname, courseId, yearId)
+VALUES
+(1, 'Esnyr', 'Ranollo', 'Dela Cruz', 123, 'Poblacion', 'Quezon City', 'Metro Manila', 'NCR', 'Philippines', '2002-06-15', 22, '123-456-7890', 'esnyr@email.com', 'Juan', 'Ranollo', 'Basketball, Reading', 'Es', 1, 2),
+(2, 'River', 'Joseph', 'Tan', 456, 'San Antonio', 'Manila', 'Metro Manila', 'NCR', 'Philippines', '2001-09-25', 23, '234-567-8901', 'riverj@email.com', 'Maria', 'Tan', 'Swimming, Travelling', 'Riv', 2, 3),
+(3, 'Charlie', 'Fleming', 'Pineda', 789, 'Tatalon', 'Cebu City', 'Cebu', 'Central Visayas', 'Philippines', '2000-02-10', 25, '345-678-9012', 'charlief@email.com', 'Antonio', 'Fleming', 'Photography, Cooking', 'Char', 3, 1),
+(4, 'Mika', 'Salamanca', 'Alonzo', 321, 'Guadalupe Viejo', 'Davao City', 'Davao del Sur', 'Davao Region', 'Philippines', '2002-11-30', 22, '456-789-0123', 'mikas@email.com', 'Cecilia', 'Salamanca', 'Gaming, Music', 'Mik', 4, 3),
+(5, 'Dustin', 'Yu', 'Lim', 654, 'Lahug', 'Makati', 'Metro Manila', 'NCR', 'Philippines', '2001-04-18', 24, '567-890-1234', 'dustiny@email.com', 'Benjamin', 'Yu', 'Cycling, Programming', 'Dust', 1, 4);
