542433aff8ac4481152a03b23f052d1e8844cc77
britojaztine <britojaztine34@gmail.com>
2025-04-03 20:27:57 +0800

    Add files via upload

---
+++ db_connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+public class db_connection
+{
+    private string connectionString = "server=localhost; database=StudentInfoDB; uid=root; pwd=;";
+
+    public MySqlConnection GetConnection()
+    {
+        return new MySqlConnection(connectionString);
+    }
+}
